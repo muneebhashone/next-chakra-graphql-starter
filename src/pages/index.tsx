ead057c917f48849b9cb8c28848658311de6bece
muneebhashone <muneeb.hashone@gmail.com>
2023-04-12 17:34:38 +0500

    title changed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Next Chakra Starter Template</title>
+        <title>Next Chakra GraphQL Starter Template</title>
         <meta name="description" content="next chakra starter template" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
